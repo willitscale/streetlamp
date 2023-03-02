25aabf1645e2e6f9767b8bc45be4c522ef9c1933
willitscale <james@n3tw0rk.co.uk>
2023-03-02 00:49:44 +0000

    Missed deprecated warning

--- tests/Attributes/Route/MethodTest.php
+++ tests/Attributes/Route/MethodTest.php
@@ -35,7 +35,7 @@ class MethodTest extends TestCase
         $methodAnnotation->applyToController($controller);
     }
 
-    public function validRouteAnnotations(): array
+    public static function validRouteAnnotations(): array
     {
         return [
             'it should identify the GET method and assign it to the route' => [
